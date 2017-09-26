0d2bd14e80a4104bf67d9474bfabcdb8ba1a847d
Unknown <fedegur@gmail.com>
2017-09-26 21:13:53 +0200

    items-list component added to home
    Home component now retrieves data from store

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,13 @@
+// core
 import { Component, OnInit } from '@angular/core';
+// ngrx
+import { Store } from '@ngrx/store';
+// rxjs
+import { Observable } from 'rxjs/Rx';
+// models
+import { IMovies } from '../../common/models/movies.model';
+// reducers
+import * as reducer from '../../common/reducers/search.reducer';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +15,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  // data from store
+  list$: Observable<reducer.State>;
 
-  constructor() { }
+  constructor(private store: Store<reducer.State>) {
+    // subscribe to data
+    this.list$ = store.select(reducer.getSearchState);
+  }
 
   ngOnInit() {
     console.log('Home component loaded');
+  }
+
+  // selected movie id
+  setCurrentMovie(movieId): void {
+    console.log('setCurrentMovie movieId: ', movieId);
   }
 
 }
